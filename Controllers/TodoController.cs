6fe73421dc4de675ad080564995f9adf27ca9f81
tkottke90 <altotom90@gmail.com>
2017-11-10 11:48:57 -0600

    Added Push

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -37,6 +37,22 @@ namespace TodoApi.Controllers
             }
             return new ObjectResult(item);
         }       
+
+        [HttpPost]
+        public IActionResult Create([FromBody] TodoItem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+
+            _context.TodoItems.Add(item);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetTodo", new { id = item.Id }, item);
+        }
+
+        
     }
 
     
